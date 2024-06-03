01e624208fac11760668209a28bbdc34f1f3b29a
huynhchinhan <cicilian@MacBook-Air-cua-huynhchinhan.local>
2024-06-03 18:54:25 +0700

    delete stuff file

--- test.py
+++
@@ -1,4 +0,0 @@
-import h5py
-model_path = "/Users/cicilian/Desktop/Môn học/Năm II - HK 2/AI/Do_an_CK/myTesting/models/model_4/trained_model.h5"
-with h5py.File(model_path, 'r') as f:
-    print(list(f.keys()))  # In ra các nhóm dữ liệu trong tệp HDF5
